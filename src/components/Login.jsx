48942fb34d91d631257809e500ee8246dcb11104
danielzemanate <danielzemanate@gmail.com>
2021-09-22 12:59:52 -0500

    crear proyecto react, establecer arquitectura mediante componentes y creacion de rutas router dom

---
+++ src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import '../styles/login.css'
+
+export class Login extends Component {
+
+    // FUNCION PARA LOGUEARSE
+    login = () => {
+        console.log('Se hizo click');
+        window.location.href= "./Home"
+      }
+      
+    render() {
+        return (
+            <div>
+                <h1>ESTE ES EL LOGIN</h1>
+                <button className="botonGenerico mainButton" onClick={this.login}>LOGIN</button>
+            </div>
+        )
+    }
+}
+export default Login;
+
+
